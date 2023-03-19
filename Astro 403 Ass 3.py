d0e0710047507aac2491e144fdaf0a2cce1d661b
mudkip-cant-code <ywow5k3@gmail.com>
2023-03-19 15:26:24 -0600

    calculates drop correctly

--- Astro 403 Ass 3.py
+++ Astro 403 Ass 3.py
@@ -26,12 +26,12 @@ T = T_c*10**(-1.2*r/R)
 q = 2.54e6 * X**2 * rho * T**(-2/3) * np.exp(-33.81/T**(1/3))
 
 drop = False
-q_0 = q[0]/100
+q_0 = q[0]*10**-3
 
 for i in  range(r.size):
     if q[i] < q_0 and drop == False:
-        print("Dropped three orders of magnitudes at i = ", i)
-        plt.plot(r[i],q[i],'ro', label = "Dropped")
+        print("Dropped three orders of magnitudes at: ", r[i],q[i])
+        plt.plot(r[i],q[i],'ro', label = "Dropped 3 Orders of Magnitude")
         drop = True
 
 ## !PLOTING IS FUN! ## (SNORTS 3 lbs of coke) why is this not working
